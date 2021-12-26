717cc97354691c1d9de49d03390fd57fdb132b09
tilakamichael <mdt53229@uga.edu>
2021-12-26 18:12:47 -0500

    Formatting changes

--- tsa.py
+++ tsa.py
@@ -11,26 +11,28 @@ totalPolarity = 0;
 for text in tweetList:
     totalPolarity += TextBlob(text).polarity
 
+print(totalPolarity)
+
 if (totalPolarity < 0):
-    print("Twitter users are displeased with this topic, here are a few sample negative tweets")
+    print("Twitter users are displeased with this topic as it has a polarity of %d, here are a few sample negative tweets" % totalPolarity)
     i = 0
     for text in tweetList:
         if (TextBlob(text).polarity < 0):
             print(text)
+            print("\n")
             i += 1
             
         if (i == 10):
             break
         
 if (totalPolarity > 0):
-    print("Twitter users are pleased with this topic, here are a few sample positive tweets")
+    print("Twitter users are pleased with this topic as it has a polarity of %d, here are a few sample positive tweets" % totalPolarity)
     i = 0
     for text in tweetList:
         if (TextBlob(text).polarity > 0):
             print(text)
+            print("\n")
             i += 1
             
         if (i == 10):
             break
-
-print (totalPolarity)
