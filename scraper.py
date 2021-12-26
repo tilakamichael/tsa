c8e0fe3092f5740a86665f8d369f5e6f8838e707
tilakamichael <mdt53229@uga.edu>
2021-12-26 18:36:21 -0500

    Commented code

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import tweepy
 import config
-import tweepy
 
 def getClient():
+    #Initializes Tweepy client with tokens from Twitter developer account
     client = tweepy.Client(bearer_token=config.bearer_token, 
                            consumer_key=config.consumer_key,
                            consumer_secret=config.consumer_key_secret,
@@ -14,10 +14,12 @@ def getClient():
 def scrapeQuery(query, count):
     client = getClient()
     
+    #Searches recent tweets based off of parameters in tsa.py and extracts data
     tweets = client.search_recent_tweets(query=query, max_results=count)
     tweetData = tweets.data
     textList = []
     
+    #If the Tweet data exists then it adds all the data received to the list for analysis
     if not tweetData is None and len(tweetData) > 0:
         for tweet in tweetData:
             textList.append(tweet.text)
--- tsa.py
+++ tsa.py
@@ -1,23 +1,25 @@
 from textblob.en.sentiments import NaiveBayesAnalyzer
-from tweepy import tweet
 import scraper
 from textblob import TextBlob
 
+#Take topic query
 print ("Enter a topic to find polarity on: ")
 topic = input()
 
+#Scrape tweets and store in a list
 tweetList = scraper.scrapeQuery(topic, 100)
 
+#Adds polarity of 100 tweets on said topic 
 totalPolarity = 0;
 for text in tweetList:
     totalPolarity += TextBlob(text, analyzer=NaiveBayesAnalyzer()).polarity
 
-print(totalPolarity)
-
+#Prints 10 sample negative tweets if the majority of tweets are negative
 if (totalPolarity < 0):
     print("Twitter users are displeased with this topic as it has a polarity of %d, here are a few sample negative tweets" % totalPolarity)
     i = 0
     for text in tweetList:
+        #A study by JICCE found that Naive Bayes analysis was faster than that of pattern analysis using NLP
         if (TextBlob(text, analyzer=NaiveBayesAnalyzer()).polarity < 0):
             print(text)
             print("\n")
@@ -26,6 +28,7 @@ if (totalPolarity < 0):
         if (i == 10):
             break
         
+#Prints 10 sample positive tweets if the majority of tweets are positive
 if (totalPolarity > 0):
     print("Twitter users are pleased with this topic as it has a polarity of %d, here are a few sample positive tweets" % totalPolarity)
     i = 0
