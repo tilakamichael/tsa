2670c747cc134a15ed615cbfa9d19977096ba7c5
tilakamichael <mdt53229@uga.edu>
2021-12-26 16:36:08 -0500

    Added feature for users to view sample tweets

--- tsa.py
+++ tsa.py
@@ -1,3 +1,4 @@
+from tweepy import tweet
 import scraper
 from textblob import TextBlob
 
@@ -9,5 +10,27 @@ tweetList = scraper.scrapeQuery(topic, 100)
 totalPolarity = 0;
 for text in tweetList:
     totalPolarity += TextBlob(text).polarity
+
+if (totalPolarity < 0):
+    print("Twitter users are displeased with this topic, here are a few sample negative tweets")
+    i = 0
+    for text in tweetList:
+        if (TextBlob(text).polarity < 0):
+            print(text)
+            i += 1
+            
+        if (i == 10):
+            break
         
+if (totalPolarity > 0):
+    print("Twitter users are pleased with this topic, here are a few sample positive tweets")
+    i = 0
+    for text in tweetList:
+        if (TextBlob(text).polarity > 0):
+            print(text)
+            i += 1
+            
+        if (i == 10):
+            break
+
 print (totalPolarity)
